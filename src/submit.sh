5c3972519aecb5d44cac2b016196e769e1be6b39
aabccd021 <aabccd021@gmail.com>
2025-03-06 11:35:48 +0700

    Refactor: remove redundant file deletion in goto.sh and submit.sh

--- src/submit.sh
+++ src/submit.sh
@@ -315,11 +315,11 @@ if [ -f "$NETERO_DIR/url.txt" ]; then
 
 fi
 
-rm -rf "${NETERO_DIR:?}"/*
-
 eval "curl $curl_options '$url'"
 
 content_type=$(jq -r '.["content-type"][0]' ./header.json)
 if [ "$content_type" = "text/html" ]; then
   cp "$NETERO_DIR/body" "$NETERO_DIR/page.html"
+elif [ -f "$NETERO_DIR/page.html" ]; then
+  rm "$NETERO_DIR/page.html"
 fi
