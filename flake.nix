206f78404d79062cbf4d9af56bff9b3bf542adb0
aabccd021 <aabccd021@gmail.com>
2025-05-21 21:01:49 +0700

    Revert "Refactor flake.nix for formatting and dependencies."
    This reverts commit 6b56b198a5628c9e15fea421c2d87027d59f4bf6.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,13 @@
     { self, ... }@inputs:
     let
 
-      overlay = (final: prev: { netero-test = import ./src { pkgs = final; }; });
+      overlay = (
+        final: prev: {
+          netero-test = import ./src {
+            pkgs = final;
+          };
+        }
+      );
 
       pkgs = import inputs.nixpkgs {
         system = "x86_64-linux";
@@ -18,7 +24,6 @@
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
-        programs.nixfmt.strict = true;
         programs.prettier.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
@@ -35,7 +40,11 @@
 
       gotoTests = import ./test/goto { pkgs = pkgs; };
 
-      devShells.default = pkgs.mkShellNoCC { buildInputs = [ pkgs.nixd ]; };
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = [
+          pkgs.nixd
+        ];
+      };
 
       packages =
         devShells
