99a9db4bc6639556a8cdc1b5815425e5cfbbd9aa
aabccd021 <aabccd021@gmail.com>
2025-09-08 08:29:24 +0700

    Remove time-advance script and update netero-init.sh for better file handling

--- src/netero-init.sh
+++ src/netero-init.sh
@@ -7,8 +7,3 @@ fi
 if [ ! -f "$NETERO_STATE/active-tab.txt" ]; then
   printf "1" >"$NETERO_STATE/active-tab.txt"
 fi
-
-if [ ! -f "$NETERO_STATE/now.txt" ]; then
-  printf "%s" "$(date +"%Y-%m-%dT%H:%M:%SZ")" >"$NETERO_STATE/now.txt"
-  cp "$NETERO_STATE/now.txt" "$NETERO_STATE/start.txt"
-fi
--- src/time-advance.sh
+++
@@ -1,3 +0,0 @@
-time_now=$(cat "$NETERO_STATE/now.txt")
-time_advanced=$(date --utc --date "$time_now +$1" +"%Y-%m-%dT%H:%M:%SZ")
-printf "%s" "$time_advanced" >"$NETERO_STATE/now.txt"
